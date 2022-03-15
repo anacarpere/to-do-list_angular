cdd994df7adb3e3e89b34b33d1061797b0791212
anacarpere <86481653+anacarpere@users.noreply.github.com>
2022-03-15 19:40:34 -0300

    Correção de inclusão no LOcalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,41 +9,49 @@ export class AppComponent implements OnInit {
  
   title = 'project_todo';
   
-  atividades: Array<string>  = ['aula de inglês' , 'almoço' , 'tomar água', 'pagar boleto']
+  atividades: string[] | null  = []
 
-  mostrarParagrafo: boolean= false
-
-  @Input()
-  arr: string | null  = ''
- 
 
   @ViewChild('inputAtividade')
   input!: ElementRef
 
-  adicionarNome(valor:string): void {
-    this.atividades.push(valor)
-    this.input.nativeElement.value =''
-    localStorage.setItem('chave', JSON.stringify(this.atividades))   
-  }
-
-  
-
-  deletarNome(valor:number, remove:number): void {
-    this.atividades.splice(valor, remove)
-  }
-
-  deletarTudo(valor:number, remove:number): void {
-    this.atividades.splice(valor, remove)
-  }
-
   ngOnInit(): void {
-    this.arr = localStorage.getItem('chave')
-
-    if(this.arr){
-      this.atividades = JSON.parse(this.arr)
-    }
-    console.log(this.atividades)
+    this.acessarLocalStorage()
   }
+
+  incluirAtividade(valor: string): void{
+    this.atividades?.push(valor)
+    let atividadeEmString = JSON.stringify(this.atividades) //transformando array em string
+    console.log(`variavel atividadeEmString: ${atividadeEmString} ${typeof(atividadeEmString)}`)
+    localStorage.setItem('chave', atividadeEmString) // criando o storage e incluindo dados 
+    this.acessarLocalStorage() // função que passa dados para a array que aparece na tela, ela será repetida a cada novo input inserido 
+    this.input.nativeElement.value = ""
+  }
+
+  acessarLocalStorage(): void {
+    let storage = localStorage.getItem('chave') // recupera os dados que estão no Storage (em string)
+    console.log(`variavel storage ${storage} ${typeof(storage)} `)
+    let arr = JSON.parse(storage || '[]') // transforma os dados (ainda em string e transforma em um objeto)
+    console.log(`variavel arr ${arr} ${typeof(arr)} `)
+    this.atividades = arr // passa o objeto para o array de atividades que irá aparecer na tela com o ngfor
+  }
+
+  deletarAtividade(index: number, remove: number): void {
+    this.atividades?.splice(index, remove)
+    
+    let atividadeEmString = JSON.stringify(this.atividades)
+    localStorage.setItem('chave', atividadeEmString)
+    this.acessarLocalStorage()
+  }
+
+  deletarTudo(index:number):void {
+    this.atividades?.splice(index, this.atividades.length)
+
+    let atividadeEmString = JSON.stringify(this.atividades)
+    localStorage.setItem('chave',atividadeEmString)
+    this.acessarLocalStorage()
+  }
+
   
 }
 
